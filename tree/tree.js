758d06bd388c35b7c2113768454fba80a58de680
Bryan L. Venable <bryanlvenable@gmail.com>
2015-04-15 17:30:03 -0700

    Added description for tree

--- tree/tree.js
+++ tree/tree.js
@@ -1,4 +1,10 @@
 /*! tree v0.0.0 - MIT license */
+
+/* Description
+Implement a tree using prototypal instantiation.
+Must have methods to add a child and check if tree contains a value
+*/
+
 'use strict';
 
 var tree = function () {

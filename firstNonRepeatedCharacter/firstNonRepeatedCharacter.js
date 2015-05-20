e43db712aa76ec0ed106bcda539a3d8af05c19c7
Bryan L. Venable <bryanlvenable@gmail.com>
2015-05-20 16:39:17 -0700

    Started first non-repeated character

---
+++ firstNonRepeatedCharacter/firstNonRepeatedCharacter.js
@@ -0,0 +1,38 @@
+/*! firstNonRepeatedCharacter v0.0.0 - MIT license */
+'use strict';
+
+var firstNonRepeatedCharacter = function (str) {
+  /***** Pseudo Code *****/
+  // Turn string into an array
+      // Loop over array
+        // Search for current element in the 0th through length not including current term
+      // If element is found
+        // Return that element
+      // If last element is not found
+        // return null
+
+  /***** Let's rock this *****/
+  var arr = str.split('');
+  var search = function (term, arr) {
+    for (var i=0; i<term; i++) {
+      if (arr[i] === arr[term]) {
+        return true;
+      }
+    }
+    for (var i=term; i<term.length; i++) {
+      if(arr[i] === arr[term]){
+        return true;
+      }
+    }
+    return false;
+  };
+  
+  for (var i=0; i<arr.length; i++) {
+    if (search(i,arr)) {
+      return arr[i];
+    }
+  }
+  return null;
+}
+
+console.log(firstNonRepeatedCharacter('abcdca'));

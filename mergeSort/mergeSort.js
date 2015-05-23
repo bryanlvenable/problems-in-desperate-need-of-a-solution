1237eb4e3699928d5b9285563f791a5384c97cfd
Bryan L. Venable <bryanlvenable@gmail.com>
2015-05-23 14:20:26 -0700

    completed merge sort

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,6 +1,48 @@
 /*! mergeSort v0.0.0 - MIT license */
 'use strict';
 
-var mergeSort = function (value1, value2) {
-  // your code goes here
-}
+var mergeSort = function (arr) {
+  // Subroutine to merge two arrays
+  var merge = function (mergeThis, withThis) {
+    var returnedArray = [];
+    while (mergeThis.length > 0 || withThis.length > 0) {
+      if (mergeThis[0] === undefined) {
+        returnedArray.push(withThis.shift());
+      } else if (withThis[0] === undefined) {
+        returnedArray.push(mergeThis.shift());
+      } else if (mergeThis[0] <= withThis[0]) {
+        returnedArray.push(mergeThis.shift());
+      } else {
+        returnedArray.push(withThis.shift());
+      }
+    }
+    return returnedArray;
+  };
+
+  // Subroutine to turn each element of each array into an array of that element
+  var arrayEachElement = function (arrayOfElements) {
+    var results = [];
+    for (var i=0; i<arrayOfElements.length; i++) {
+      var temp = [];
+      temp.push(arrayOfElements[i]);
+      results.push(temp);
+    }
+    return results;
+  }
+
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  var arrayOfArrays = arrayEachElement(arr);
+
+  while (arrayOfArrays.length > 1) {
+    console.log(arrayOfArrays);
+    arrayOfArrays.push(merge(arrayOfArrays[0], arrayOfArrays[1]));
+    arrayOfArrays.shift();
+    arrayOfArrays.shift();
+  }
+
+  return arrayOfArrays[0];
+};
+
--- mergeSort/spec/mergeSort.js
+++ mergeSort/spec/mergeSort.js
@@ -8,13 +8,20 @@ describe('mergeSort()', function () {
 
   });
 
-  it('add 1 plus 2', function () {
-    expect(mergeSort(1,2)).to.equal(3);
+  it('return empty array if input is empty', function () {
+    expect(mergeSort([])).to.eql([]);
   });
 
-  it('does something else', function () {
-    expect(true).to.equal(false);
+  it('should sort a reversed array', function () {
+    expect(mergeSort([6,5,4,3,2,1])).to.eql([1,2,3,4,5,6]);
   });
 
-  // Add more assertions here
+  it('should sort a shuffled array', function () {
+    expect(mergeSort([4,3,6,5,1,2])).to.eql([1,2,3,4,5,6]);
+  });
+
+  it('should sort an odd indexed shuffled array', function () {
+    expect(mergeSort([4,3,5,1,2])).to.eql([1,2,3,4,5]);
+  });
+
 });
